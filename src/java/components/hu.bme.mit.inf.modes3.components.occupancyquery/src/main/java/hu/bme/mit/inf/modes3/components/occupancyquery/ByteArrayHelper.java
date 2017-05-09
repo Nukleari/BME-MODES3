88f1edc4f7b7f75127aef1d1b5c8ec5e13e630a4
edinapap <edinapap95@gmail.com>
2017-05-09 16:55:18 +0200

    Occupancy jumps bytes

--- src/java/components/hu.bme.mit.inf.modes3.components.occupancyquery/src/main/java/hu/bme/mit/inf/modes3/components/occupancyquery/ByteArrayHelper.java
+++ src/java/components/hu.bme.mit.inf.modes3.components.occupancyquery/src/main/java/hu/bme/mit/inf/modes3/components/occupancyquery/ByteArrayHelper.java
@@ -1,15 +1,67 @@
 package hu.bme.mit.inf.modes3.components.occupancyquery;
 
+import java.math.BigInteger;
+
+
 public class ByteArrayHelper {
+	
+	static byte[] bytes = new byte[]{0,0,0,1};
 
 	public static byte[] read() {
-		return new byte[]{
-				0,
-				0,
-				0,
-				1,
-		};
+		return bytes;
 	}
+	
 
 	
-}
+	public static byte[] shift(){
+		
+		
+		byte b0 = bytes[0];
+		System.arraycopy(bytes, 1, bytes, 0, bytes.length -1);
+		bytes[bytes.length -1] = b0;
+		for (byte b : bytes) {
+			System.out.print(String.format("%x", b));
+		}
+		return bytes;
+		
+		
+//		
+//		System.out.println(bytes.length);
+//		byte[] c = null;
+//		for(int i= 0; i< bytes.length; i++){
+//		
+//		//	start[i] << 1;
+//		
+//			byte b = bytes[i];
+//			b = (byte) (b << 1);
+//			c[i] = b;
+//		}		
+//		for (byte b : c) {
+//	    System.out.print(String.format("%x", b));
+//	}
+//		return c;
+		
+		
+		
+		
+		
+//		
+////			byte[] barray = read();
+//////			for (byte b : barray) {
+//////			    System.out.print(String.format("%x", b));
+//////			}
+////			BigInteger bigInt = new BigInteger(barray);
+////
+////			// shift
+////			BigInteger shiftInt = bigInt.shiftRight(4);
+////			
+////			// back to array
+////			 byte[] shifted = shiftInt.toByteArray();
+////			 for (byte b : shifted) {
+////				    System.out.print(String.format("%x", b));
+////				}
+////			return shifted;
+	}
+	}
+	
+
--- src/java/components/hu.bme.mit.inf.modes3.components.occupancyquery/src/main/java/hu/bme/mit/inf/modes3/components/occupancyquery/FakeOccupancyReader.xtend
+++ src/java/components/hu.bme.mit.inf.modes3.components.occupancyquery/src/main/java/hu/bme/mit/inf/modes3/components/occupancyquery/FakeOccupancyReader.xtend
@@ -1,23 +1,35 @@
 package hu.bme.mit.inf.modes3.components.occupancyquery
 
+
+
 class FakeOccupancyReader implements Runnable {
 	def byte[] read() {
 		val bytes = ByteArrayHelper.read
 		
-		bytes.set(0, (bytes.get(0) >>> 1) as byte);
+	//	bytes.set(0, (bytes.get(0) >>> 1) as byte);
 		
 		return bytes
 	}
 
+//	def byte[] shift(){
+//		val bytes = ByteArrayHelper.shift
+//		bytes.set(0, (bytes.get(0) >>> 1) as byte);
+//		
+//		return bytes
+//	}
+	
+
 	override run() {
-		// TODO generate byte array
+		// generate byte array
 		// Create a 4 byte array, shift a single 1 from index 0 to index 31 every 1 sec
+		while(true){
+			Thread.sleep(1000)		
+			ByteArrayHelper.shift()
+			read()
+			
+			}
+		
 	}
 	
-//	plz help, I'm stucked :(
-//	howto byte tömb :(
-//	ez java helper kéne legyen. de hogyan? :(
-//	public static byte[] byteArray(int length) {
-//  return new byte[length];
-//}
+
 }
